5ff48d883fc6b3d70ab15fbbf76344abe60096f0
sasriawesome <sasri.project@gmail.com>
2020-02-14 18:47:39 +0700

    Setup SMTP Email Backend

--- intradarmajaya/settings/base.py
+++ intradarmajaya/settings/base.py
@@ -234,4 +234,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "sasri.djproject@gmail.com"
-EMAIL_HOST_PASSWORD = "habibie099"
+EMAIL_HOST_PASSWORD = "eelhfcfhvoxjunxk"
