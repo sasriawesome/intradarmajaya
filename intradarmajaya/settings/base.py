ed9032e796bde2dc6e8d68d1f72855e9e0f7edf6
sasriawesome <sasri.project@gmail.com>
2020-03-16 23:43:50 +0700

    comment environ

--- intradarmajaya/settings/base.py
+++ intradarmajaya/settings/base.py
@@ -32,7 +32,7 @@ WAGTAIL_SITE_NAME = "intradarmajaya"
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
-environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
